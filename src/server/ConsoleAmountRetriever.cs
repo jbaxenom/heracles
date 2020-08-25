8f9869d48ea39d70c35a7ac816a9f7df3b15a5f7
Chema del Barco <jbaxenom@gmail.com>
2020-08-25 12:40:09 +0200

    Forgot ConsoleAmountRetriever

---
+++ src/server/ConsoleAmountRetriever.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace server
+{
+    class ConsoleAmountRetriever
+    {
+        public virtual decimal GetAmount()
+        {
+            return decimal.Parse(Console.ReadLine());
+        }
+    }
+}
