04a142f8d2b3b6bcf1b4989bf165a0b7f7ff7e8f
Chema del Barco <jbaxenom@gmail.com>
2020-08-27 17:17:47 +0200

    Created basic ASP.NET Web API using VS templates

---
+++ src/heracles.webapp/Controllers/MoneyController.cs
@@ -0,0 +1,38 @@
+﻿using heracles.console;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace heracles.webapp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MoneyController : ControllerBase
+    {
+        private readonly ILogger<MoneyController> _logger;
+
+        public MoneyController(ILogger<MoneyController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{input}")]
+        public ActionResult<string> GetFormattedMoney(string input)
+        {
+            string output;
+            try
+            {
+                output = new MoneyFormatter().Format(input);
+            }
+            catch (ArgumentException e)
+            {
+                output = e.Message;
+            }
+
+            var logMessage = $"Requested input '{input}', Heracles responded with '{output}'";
+            _logger.LogInformation(logMessage);
+
+            return output;
+        }
+    }
+}
